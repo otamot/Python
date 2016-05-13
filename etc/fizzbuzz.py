497e13ca790dab21e4d8f1298b83a67361488639
otamot <tomo_otamot@yahoo.co.jp>
2016-05-13 11:05:39 +0900

    add fizzbuzz programming

--- etc/fizzbuzz.py
+++
@@ -1,6 +0,0 @@
-for i in range(1,100):
-    if((i%3==0&i%5==0)==False):print(str(i))
-    else:
-        if(i%3==0):print ("Fizz",end="")
-        if(i%5==0):print ("Buzz",end="")
-        print ("")
